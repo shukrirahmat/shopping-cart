504988e44193e7bd52db51314f2693965ec7c4bd
Shukri <119567950+shukrirahmat@users.noreply.github.com>
2024-10-01 17:37:12 +0800

    add heade styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
-import { Link, useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom";
 import Home from "./components/Home";
 import Shop from "./components/Shop";
 import Cart from "./components/Cart";
+import styles from "./App.module.css";
 
 function App() {
-  const {name} = useParams();
+  const { name } = useParams();
 
   return (
-    <>
-    <nav>
-      <Link to="/">HOME</Link>
-      <Link to="/shop">SHOP</Link>
-      <Link to="/cart">CART</Link>
-    </nav>
-    <div>
-      {name === "shop"? (
-        <Shop/>
-      ) : name === "cart"? (
-        <Cart/>
-      ) : (
-        <Home/>
-      )}
+    <div className={styles.background}>
+      <div className={styles.header}>
+        <div className={styles.headWrapper}>
+        <p className={styles.logo}>Yangtze</p>
+        <nav className={styles.linksWrapper}>
+          <Link to="/" className={styles.link}>HOME</Link>
+          <Link to="/shop" className={styles.link}>SHOP</Link>
+          <Link to="/cart" className={styles.link}>CART</Link>
+        </nav>
+        </div>
+      </div>
+      <div>
+        {name === "shop" ? <Shop /> : name === "cart" ? <Cart /> : <Home />}
+      </div>
     </div>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
