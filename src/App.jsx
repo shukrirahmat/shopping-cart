fa14efcb3ce57b64eac679af825da10ed99aee99
Shukri <119567950+shukrirahmat@users.noreply.github.com>
2024-10-03 18:32:43 +0800

    add fetching from shop api

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,56 @@ import Shop from "./components/Shop";
 import Cart from "./components/Cart";
 import styles from "./App.module.css";
 import "./index.css";
+import { useEffect, useState } from "react";
+
+function useItemsData() {
+  const [items, setItems] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products", { mode: "cors" })
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error("server error");
+        }
+        return response.json();
+      })
+      .then((response) => setItems(response))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return {items, error, loading}
+}
 
 function App() {
   const { name } = useParams();
+  const {items, error, loading} = useItemsData();
+
+  if (error)
+    return <p className={styles.errortext}>A network error was encountered.</p>;
 
   return (
     <div className={styles.background}>
       <div className={styles.header}>
         <div className={styles.headWrapper}>
-        <p className={styles.logo}>Yangtze</p>
-        <nav className={styles.linksWrapper}>
-          <Link to="/" className={styles.link}>HOME</Link>
-          <Link to="/shop" className={styles.link}>SHOP</Link>
-          <Link to="/cart" className={styles.link}>CART</Link>
-        </nav>
+          <p className={styles.logo}>Yangtze</p>
+          <nav className={styles.linksWrapper}>
+            <Link to="/" className={styles.link}>
+              HOME
+            </Link>
+            <Link to="/shop" className={styles.link}>
+              SHOP
+            </Link>
+            <Link to="/cart" className={styles.link}>
+              CART
+            </Link>
+          </nav>
         </div>
       </div>
       <div>
-        {name === "shop" ? <Shop /> : name === "cart" ? <Cart /> : <Home />}
+        {name === "shop" ? <Shop items={items} loading={loading}/> : name === "cart" ? <Cart /> : <Home />}
       </div>
     </div>
   );
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,7 +1,34 @@
-function Shop() {
+import PropTypes from "prop-types";
+import styles from "./Shop.module.css";
+
+function Shop(props) {
+  if (props.loading) {
     return (
-        <div>THIS IS SHOP PAGE</div>
-    )
+      <div className={styles.wrapper}>
+        <p className={styles.loadingText}>LOADING...</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.itemContainer}>
+        {props.items.map((item) => {
+            return (
+                <div className={styles.itemCard} key={item.id}>
+                    <p>{item.title}</p>
+                    <img src={item.image}></img>
+                </div>
+            )
+        })}
+      </div>
+    </div>
+  );
 }
+
+Shop.propTypes = {
+  items: PropTypes.array,
+  loading: PropTypes.bool,
+};
 
 export default Shop;
