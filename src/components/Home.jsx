ee0198f0ae3dfbee59715026524c26db1eb39ce1
Shukri <119567950+shukrirahmat@users.noreply.github.com>
2024-10-04 13:14:54 +0800

    add little scrollup function when go to shop

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,13 +4,21 @@ import "../index.css"
 import styles from "./Home.module.css";
 
 function Home() {
+
+    function handleScrollUp() {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    }
+
     return (
         <div className={styles.wrapper}>
             <img src={photo} alt="Store Photo" className={styles.photo}/>
             <div className={styles.welcomeText}>
                 <p class={styles.title}><b>Shops online with fast and reliable delivery</b></p>
                 <p>Made from top-tier materials, check out of our high quality products! </p>
-                <Link to="/shop" className={styles.shopBtn}>Shop now</Link>
+                <Link to="/shop" className={styles.shopBtn} onClick={handleScrollUp}>Shop now</Link>
             </div>
         </div>
     )
