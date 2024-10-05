8a475c5b84075f760c167fb18afe236946029b94
Shukri <119567950+shukrirahmat@users.noreply.github.com>
2024-10-05 12:29:38 +0800

    done input amount handling

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,26 +1,47 @@
 import PropTypes, { func } from "prop-types";
 import styles from "./ItemCard.module.css";
+import { useState } from "react";
 
-function ItemCard({item}) {
-    return (
-        <div className={styles.card}>
-          <p className={styles.cardTitle}>{item.title}</p>
-          <img src={item.image}></img>
-          <div className={styles.priceandamount}>
-            <p className={styles.cardPrice}>{item.price}$</p>
-            <div className={styles.amount}>
-              <button>-</button>
-              <input type="text" value="1"></input>
-              <button>+</button>
-            </div>
-          </div>
-          <button className={styles.addtoCart}>ADD TO CART</button>
+function ItemCard({ item }) {
+  const [amount, setAmount] = useState(1);
+
+  function changeAmount(e) {
+    const value = e.target.value.replace(/[^0-9]/g, "");
+    setAmount(value);
+  }
+
+  function handleBlur() {
+    if (!amount) setAmount(1);
+    if (amount > 99) setAmount(99);
+  }
+
+  function increaseAmount() {
+    if (amount < 99) setAmount(amount + 1);
+  }
+
+  function decreaseAmount() {
+    if (amount > 1) setAmount(amount - 1);
+  }
+
+  return (
+    <div className={styles.card}>
+      <p className={styles.cardTitle}>{item.title}</p>
+      <img src={item.image}></img>
+      <div className={styles.priceandamount}>
+        <p className={styles.cardPrice}>{item.price}$</p>
+        <div className={styles.amount}>
+          <button onClick={decreaseAmount}>-</button>
+          <input type="text" value={amount} onBlur={handleBlur} onChange={changeAmount}></input>
+          <button onClick={increaseAmount}>+</button>
         </div>
-    );
+      </div>
+      <button className={styles.addtoCart}>ADD TO CART</button>
+    </div>
+  );
 }
 
 ItemCard.proptypes = {
-    item: PropTypes.object
-}
+  item: PropTypes.object,
+};
 
 export default ItemCard;
